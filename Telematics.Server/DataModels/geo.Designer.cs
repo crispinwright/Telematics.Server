91afd993fe5f09f8ea66cec0c7258fab39fd5539
Crispin <crispin@blocksoft.co.uk>
2013-11-01 14:33:21 +1300

    linked to azure db and fixed ef issues

--- Telematics.Server/DataModels/geo.Designer.cs
+++ Telematics.Server/DataModels/geo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\Telematics.Server\Telematics.Server\DataModels\geo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Crispin\documents\visual studio 2013\Projects\Telematics.Server\Telematics.Server\DataModels\geo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
