171ba403d440e61047c0bee29ac305f36c6f190e
Crispin <crispin@blocksoft.co.uk>
2013-11-04 13:14:24 +1300

    adding source files to git

---
+++ Telematics.Server.ServiceLayer/GeoService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telematics.Server.Data.DataModels;
+
+namespace Telematics.Server.ServiceLayer
+{
+    public class GeoService : IGeoService
+    {
+
+        public void AddGeoUserPoints(GEOMain geoData)
+        {
+            var context = new geoEntities();
+            //var user = context.Users.Where(x => x.ID == geoData.PointTable.UserID);
+            geoData.PointTable.Points.ToObservable().Subscribe(i =>
+                context.VehicleSpeeds.Add(
+                    new VehicleSpeed
+                    {
+                        Lat = i.Lat,
+                        Lon = i.Lon,
+                        Speed = i.Speed,
+                        UserID = geoData.PointTable.UserID,
+                        VehicleID = geoData.PointTable.VehicleID
+                    }));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Telematics.Server.ServiceLayer/IGeoService.cs
@@ -0,0 +1,7 @@
+namespace Telematics.Server.ServiceLayer
+{
+    public interface IGeoService
+    {
+        void AddGeoUserPoints(GEOMain geoData);
+    }
+}
