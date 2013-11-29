8b8e346fa439b4e4c2b44378c3d04a204bb61f0e
Crispin <crispin@blocksoft.co.uk>
2013-11-29 23:25:52 +1300

    adding user service interface

---
+++ Telematics.Server.ServiceLayer/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telematics.Server.Data.Json;
+
+namespace Telematics.Server.ServiceLayer
+{
+    public interface IUserService
+    {
+
+        IEnumerable<User> GetUsers();
+
+    }
+}
