a091636ffd0d1ed2b26a778bcfaadc87e34ee175
crispinwright <crispinwright@gmail.com>
2014-01-17 15:50:37 +1300

    making it work again

--- geoDb/dbo/Tables/VehicleSpeed.sql
+++ geoDb/dbo/Tables/VehicleSpeed.sql
@@ -1,21 +1,21 @@
 ﻿CREATE TABLE [dbo].[VehicleSpeed](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[UserID] [int] NOT NULL,
-	[DeviceID] [VARCHAR] NULL,
+    [DeviceID] varchar(10)  NULL,
 	[VehicleID] [int] NOT NULL,
 	[Lat] [float] NULL,
 	[Lon] [float] NULL,
-	[Speed] [float] NULL,
-	[SpeedLimit] [float] NULL,
-	[Distance] [float] NULL,
-	[EventTime] [timestamp] NULL,
-	[UTCTime] [datetime] NULL,
- [Route] NVARCHAR(MAX) NULL, 
+    [Speed] [float]  NULL,
+    [EventTime] binary(8)  NULL,
+    [UTCTime] datetime  NULL,
+    [SpeedLimit] float  NULL,
+    [Distance] float  NULL,
+    [Route] nvarchar(max)  NULL
     CONSTRAINT [PK_VehicleSpeed] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
 GO
 ALTER TABLE [dbo].[VehicleSpeed]  WITH CHECK ADD  CONSTRAINT [FK_VehicleSpeed_User] FOREIGN KEY([UserID])
 REFERENCES [dbo].[User] ([ID])
