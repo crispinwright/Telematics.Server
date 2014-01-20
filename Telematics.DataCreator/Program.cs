840fb346146aa5845629557d1e5858531352d445
kmcc049 <kyle.mylastname@gmail.com>
2014-01-20 16:21:43 +1300

    smaller interval and a group size of 1

--- Telematics.DataCreator/Program.cs
+++ Telematics.DataCreator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -30,9 +31,20 @@ namespace Telematics.DataCreator
 				points.Add(new Tuple<double, double>(double.Parse(lat), double.Parse(lon)));
 			}
 
-			var interval = 1000*15;
-			var groupsize = 4;
-			HttpClient cl = new HttpClient {BaseAddress = new Uri("http://ktelematics.azurewebsites.net/")};
+			var interval = 1000*5;
+			var groupsize = 1;
+//			HttpClient cl = new HttpClient {BaseAddress = new Uri("http://ktelematics.azurewebsites.net/")};
+
+            var handler = new HttpClientHandler
+            {
+                //                    CookieContainer = cookies,
+                //                    UseCookies = true,
+                UseDefaultCredentials = false,
+                //Credentials = ,
+                Proxy = new WebProxy("http://w8dvaklpx01", false, new string[] { },new NetworkCredential("si554437","")),
+                UseProxy = true,
+            };
+            HttpClient cl = new HttpClient(handler) { BaseAddress = new Uri("http://ktelematics3.azurewebsites.net/") };
 			//	cl.BaseAddress = new Uri("http://localhost/telematics.server/");
 			cl.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 			for (int i = 0; i < points.Count/groupsize; i++)
@@ -46,7 +58,7 @@ namespace Telematics.DataCreator
 					                {
 						                Lat = points[start + counter].Item1,
 						                Lon = points[start + counter].Item2,
-						                UTCTime = DateTime.UtcNow
+						                UTCTime = DateTime.UtcNow.AddYears(1000)
 					                };
 					        counter++;
 					        return result;
